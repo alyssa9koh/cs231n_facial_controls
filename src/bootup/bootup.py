0b93e1d596ecfd7d07bd389c32246ee6445bed4a
Alyssa Koh <a9koh@stanford.edu>
2024-05-31 12:35:42 -0700

    basic module structure

---
+++ src/bootup/bootup.py
@@ -0,0 +1,7 @@
+import pyautogui
+
+def start():
+    print('-----')
+    print('Project: CS231N Facial Controls starting...')
+    print('')
+    print('Calibrating to your screen...')
--- src/main.py
+++ src/main.py
@@ -1,5 +1,20 @@
+import pyautogui
+
+from bootup.bootup import *
+
+
+# Get the size of the primary monitor
+screen_width, screen_height = pyautogui.size()
+# Calculate the center of the screen
+center_x = screen_width // 2
+center_y = screen_height // 2
+
+
 def main():
-    print('poop')
+    start()
+    print('meow')
+
 
 if __name__ == "__main__":
     main()
+
