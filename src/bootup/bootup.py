6527719666e988d1c67c52055924c25207d43497
Alyssa Koh <a9koh@stanford.edu>
2024-05-31 12:44:03 -0700

    basic bootup added

--- src/bootup/bootup.py
+++ src/bootup/bootup.py
@@ -3,8 +3,21 @@ import pyautogui
 # Defines what functions should be exported
 __all__ = ['start']
 
+def screen_calibration():
+    print('Calibrating to your screen...')
+    # Get the size of the primary monitor
+    screen_width, screen_height = pyautogui.size()
+    # Calculate the center of the screen
+    center_x = screen_width // 2
+    center_y = screen_height // 2
+    print('...')
+    print('Moving the cursor to the middle of the screen.')
+    pyautogui.moveTo(center_x, center_y, duration=1)
+    print('Your screen is now calibrated!')
+    return center_x, center_y
+
 def start():
     print('-----')
     print('Project: CS231N Facial Controls starting...')
     print('')
-    print('Calibrating to your screen...')
+    center_x, center_y = screen_calibration()
--- src/main.py
+++ src/main.py
@@ -2,18 +2,8 @@ import pyautogui
 
 from bootup import *
 
-
-# Get the size of the primary monitor
-screen_width, screen_height = pyautogui.size()
-# Calculate the center of the screen
-center_x = screen_width // 2
-center_y = screen_height // 2
-
-
 def main():
     start()
-    print('meow')
-
 
 if __name__ == "__main__":
     main()
